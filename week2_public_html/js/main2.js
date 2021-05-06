9ed8e29ef3e4384727d6b21c085b32401449b5d2
Karim <karim.moubarik@metropolia.fi>
2021-05-06 13:36:44 +0300

    for prod

--- week2_public_html/js/main2.js
+++ week2_public_html/js/main2.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+const url = 'https://10.114.34.7:3000'; // change url when uploading to server
 
 // select existing html elements
 const addForm = document.querySelector('#addCatForm');
