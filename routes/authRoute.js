cc7518a0a2cd53afe9a855a5aadd817ad509f3fc
Karim <karim.moubarik@metropolia.fi>
2021-05-06 14:38:08 +0300

    uusin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 router.post('/login', authController.login);
 router.post('/register', body('name').isLength({min: 3}).notEmpty().trim().escape(),
@@ -15,8 +16,8 @@ router.post('/register', body('name').isLength({min: 3}).notEmpty().trim().escap
         return res.status(400).json({errors: errors.array()})
       }
 
- //     const salt = bcrypt.genSaltSync(12);
- //     user['password'] = bcrypt.hashSync(req.body.password, salt);
+      const salt = bcrypt.genSaltSync(12);
+      user['password'] = bcrypt.hashSync(req.body.password, salt);
       userController.user_create(req, res);
     });
 
