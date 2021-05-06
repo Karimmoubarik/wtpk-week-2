c2d1791254d2da20ba855543a4ac17a03e1c7934
Karim <karim.moubarik@metropolia.fi>
2021-05-06 13:39:34 +0300

    odkfod

--- week2_public_html/js/main4.js
+++ week2_public_html/js/main4.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'https://10.114.34.7:3000'; // change url when uploading to server
+const url = 'http://10.114.34.7:3000'; // change url when uploading to server
 
 // select existing html elements
 const loginWrapper = document.querySelector('#login-wrapper');
