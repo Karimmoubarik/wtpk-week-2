962430587d677170d9019ad0feff7efe7f74224f
Karim <karim.moubarik@metropolia.fi>
2021-05-06 13:48:13 +0300

    djfi

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 router.post('/login', authController.login);
+router.post('/register', body('name').isLength({min: 3}).notEmpty().trim().escape(),
+    body('email').isEmail().trim().escape(),
+    body('password').isLength({min: 8}).matches('(?=.*[A-Z])').trim().escape(),
+    (req, res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+      }
+      userController.user_create(req, res);
+    });
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,16 +6,7 @@ const {body, validationResult} = require('express-validator')
 const router = express.Router();
 
 router.get('/', userController.user_list_get);
-router.post('/', body('name').isLength({min: 3}).notEmpty().trim().escape(),
-    body('email').isEmail().trim().escape(),
-    body('password').isLength({min: 8}).matches('(?=.*[A-Z])').trim().escape(),
-    (req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-          return res.status(400).json({errors: errors.array()})
-        }
-        userController.user_create(req, res);
-    });
+
 
 router.get('/:id', userController.user_get_by_id);
 router.put('/:id', userController.user_update);
